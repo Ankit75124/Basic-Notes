ac9c240e9171d9aef9b7144476c2bd604dc724d3
devil07625 <antivirus076@gmail.com>
2022-09-22 23:47:11 +0530

    3rd Deploy React app to GitHub Pages

---
+++ precache-manifest.a2b66b4d936a4e7a5f0835e0a88fff4d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eecbd24cbac76fa60b28ddf41d3b26f",
+    "url": "/Basic-Notes/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/Basic-Notes/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "585832a3ad254477d780",
+    "url": "/Basic-Notes/static/js/main.133698b9.chunk.js"
+  },
+  {
+    "revision": "792e531396332944fe10",
+    "url": "/Basic-Notes/static/js/runtime-main.7bcea9aa.js"
+  }
+]);
